a9358537198584e218f4e3582b9742182d1235bc
Shiuan Chen <180915555+ConsciousnessOnly@users.noreply.github.com>
2024-10-26 19:28:53 +0800

    feat: getComputerChoice()
    Randomly return one of the following string values:
      “rock”, “paper” or “scissors”.

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+// console.log("Hello World");
+const log = console.log;
+
+function getComputerChoice(){
+    const number = Math.random() * 100;
+    log(number);
+
+    if (number <= 33){
+        return "rock"
+    }
+    else if (number <= 66){
+        return "paper"
+    }
+    else{
+        return "scissors"
+    }
+}
+
+function testComputerChoice(){
+    let count = 1;
+    while(count <= 30){
+        log(getComputerChoice());
+        count++;
+    }
+}
+// testComputerChoice();
+// log(getComputerChoice());
+
