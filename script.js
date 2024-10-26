27aef445da8cc2c0d7e45d2c554ee227fce34cf5
Shiuan Chen <180915555+ConsciousnessOnly@users.noreply.github.com>
2024-10-27 00:19:05 +0800

    feat: determine winner and display winning message

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ let computerScore = 0;
 
 function getComputerChoice(){
     const number = Math.random() * 100;
-    log(number);
+    log("Random number is : " + number);
 
     if (number <= 33){
+        log("Computer choose: rock");
         return "rock"
     }
     else if (number <= 66){
+        log("Computer choose: paper");
         return "paper"
     }
     else{
+        log("Computer choose: scissors");
         return "scissors"
     }
 }
@@ -30,11 +33,11 @@ function testComputerChoice(){
 
 function getHumanChoice(){
     let humanSelection = prompt("Please choose from Rock, Paper, Scissors",'');
-    log("Before change: " + humanSelection);
+    log("You choose(before change): " + humanSelection);
 
     humanSelection = (humanSelection === null) ? getHumanChoice() : 
                     humanSelection.toLowerCase();
-    log("After change: " + humanSelection);    
+    log("You choose(after change): " + humanSelection);
     
     switch (humanSelection){
          case "rock":
@@ -47,3 +50,47 @@ function getHumanChoice(){
 }
 // getHumanChoice();
 // log("You choose: " + getHumanChoice());
+
+function playRound(humanChoice, computerChoice){
+    const computerWin = "Computer win! ";
+    const humanWin = "You win! "
+    const rockWin = "Rock beats Scissors";
+    const paperWin = "Paper beats Rock";
+    const scissorsWin = "Scissors beats Paper";    
+
+    if (humanChoice === computerChoice){
+        log("It’s a tie; play again.");
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+    else if (humanChoice === "rock"){
+        switch (computerChoice){
+            case "paper":
+                log(computerWin + paperWin);
+                break;
+            case "scissors":
+                log(humanWin + rockWin);
+                break;
+        }
+    }
+    else if (humanChoice === "paper"){
+        switch (computerChoice){
+            case "scissors":
+                log(computerWin + scissorsWin);
+                break;
+            case "rock":
+                log(humanWin + paperWin);
+                break;
+        }
+    }
+    else if (humanChoice === "scissors"){
+        switch (computerChoice){
+            case "rock":
+                log(computerWin + rockWin);
+                break;
+            case "paper":
+                log(humanWin + paperWin);
+                break;
+        }
+    }
+}
+// playRound("paper", "paper");
