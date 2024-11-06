10618c955135951fdf2afd00a679ff51637d1d01
Shiuan Chen <180915555+ConsciousnessOnly@users.noreply.github.com>
2024-11-06 21:15:49 +0800

    feat: play the game by clicking on buttons
    - Create three buttons, one for each selection
    - Add an event listener to the buttons that call playRound function
      with the correct player selection every time a button is clicked

--- script.js
+++ script.js
@@ -1,132 +1,132 @@
 const log = console.log;
 
-playGame();
+const btnRock = document.querySelector("#rock");
+btnRock.addEventListener("click", () => {
+    log("You choose: ✊🏻");
+    playRound("rock");
+});
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+const btnPaper = document.querySelector("#paper");
+btnPaper.addEventListener("click", () => {
+    log("You choose: 🖐🏻");
+    playRound("paper");
+});
 
-    startRound();
-    displayWinner(humanScore, computerScore);
+const btnScissors = document.querySelector("#scissors");
+btnScissors.addEventListener("click", () => {
+    log("You choose: ✌🏻");
+    playRound("scissors");
+});
 
-    function startRound() {
-        let round = 1;
+let humanScore = 0;
+let computerScore = 0;
 
-        while (round <= 5) {
-            console.group(`Round: ${round}`)
-            playRound();
-            console.groupEnd(`Round: ${round}`)
-            round++;
+function displayWinner(humanScore, computerScore) {
+    console.group("😇 Final score 😇")
+    log(`👶 Human: ${humanScore} vs 💻 Computer: ${computerScore}`);
+
+    if (humanScore + computerScore === 5) {
+        if (humanScore > computerScore) {
+            log("🏆 The Winner is: Human! 🏆");
+        }
+        else if (computerScore > humanScore) {
+            log("🏆 The Winner is: Computer! 🏆");
+        }
+    }
+    else {
+        log("What's going on? 😱 The total scores (human + computer) must equal 5...");
+    }
+    console.groupEnd("😇 Final score 😇");
+} 
+
+function playRound(humanChoice, computerChoice = getComputerChoice()) {
+    if (isEmpty(humanChoice) || isEmpty(computerChoice)) {
+        log("Input is empty! Try again.");
+        // playRound();            
+    }
+    else if (humanChoice === computerChoice) {
+        log("It’s a tie; play again.");
+        // playRound();
+    }
+    else {
+        switch (
+        isRock(humanChoice) && isScissors(computerChoice) ||
+        isPaper(humanChoice) && isRock(computerChoice) ||
+        isScissors(humanChoice) && isPaper(computerChoice)) {
+            case true:
+                showWinner("You win!", humanChoice, computerChoice);
+                addScore("human");
+                showCurrentScore();
+                break;
+            case false:
+                showWinner("Computer win!", computerChoice, humanChoice);
+                addScore("computer");
+                showCurrentScore();
         }
     }
 
-    function displayWinner(humanScore, computerScore) {
-        console.group("😇 Final score 😇")
-        log(`👶 Human: ${humanScore} vs 💻 Computer: ${computerScore}`);
-
-        if (humanScore + computerScore === 5) {
-            if (humanScore > computerScore) {
-                log("🏆 The Winner is: Human! 🏆");
-            }
-            else if (computerScore > humanScore) {
-                log("🏆 The Winner is: Computer! 🏆");
-            }
-        }
-        else {
-            log("What's going on? 😱 The total scores (human + computer) must equal 5...");
-        }
-        console.groupEnd("😇 Final score 😇");
+    function showWinner(msgWinner, winnerChoice, loserChoice) {
+        log(`${msgWinner} ${winnerChoice} bests ${loserChoice}`);
     }
 
-    function playRound(humanChoice = getHumanChoice(), computerChoice = getComputerChoice()) {
-        if (isEmpty(humanChoice) || isEmpty(computerChoice)) {
-            log("Input is empty! Try again.");
-            playRound();
-        }
-        else if (humanChoice === computerChoice) {
-            log("It’s a tie; play again.");
-            playRound();
-        }
-        else {
-            switch (
-            isRock(humanChoice) && isScissors(computerChoice) ||
-            isPaper(humanChoice) && isRock(computerChoice) ||
-            isScissors(humanChoice) && isPaper(computerChoice)) {
-                case true:
-                    showWinner("You win!", humanChoice, computerChoice);
-                    addScore("human");
-                    showCurrentScore();
-                    break;
-                case false:
-                    showWinner("Computer win!", computerChoice, humanChoice);
-                    addScore("computer");
-                    showCurrentScore();
-            }
-        }
-
-        function showWinner(msgWinner, winnerChoice, loserChoice) {
-            log(`${msgWinner} ${winnerChoice} bests ${loserChoice}`);
-        }
-
-        function showCurrentScore() {
-            log(`Human: ${humanScore} vs Computer: ${computerScore}`);
-        }
-
-        function addScore(identity) {
-            identity === "human" ? humanScore += 1 : computerScore += 1;
-        }
+    function showCurrentScore() {
+        log(`Human: ${humanScore} vs Computer: ${computerScore}`);
     }
 
-    function getComputerChoice() {
-        const number = Math.floor(Math.random() * 3);
-        // log("Random number is : " + number);
-
-        switch (number) {
-            case 0:
-                log("Computer choose: ✊🏻");
-                return "rock"
-            case 1:
-                log("Computer choose: 🖐🏻");
-                return "paper"
-            case 2:
-                log("Computer choose: ✌🏻");
-                return "scissors"
-        }
+    function addScore(identity) {
+        identity === "human" ? humanScore += 1 : computerScore += 1;
     }
-   
-    function getHumanChoice() {
-        let humanChoice = prompt("Please choose from Rock, Paper, Scissors", '');
+}
 
-        if (isEmpty(humanChoice)) {
-            log('You Input Empty, please Input again.');
-            return getHumanChoice();
-        }
-        else if (
-            isRock(humanChoice) ||
-            isPaper(humanChoice) ||
-            isScissors(humanChoice)) {
-            log(`You Input ${humanChoice}.`);
-            return humanChoice.toLowerCase();
-        }
-        else {
-            log("Input again!");
-            return getHumanChoice();
-        }
-    }
+function getComputerChoice() {
+    const number = Math.floor(Math.random() * 3);
+    // log("Random number is : " + number);
 
-    function isEmpty(string) {
-        return !string
+    switch (number) {
+        case 0:
+            log("Computer choose: ✊🏻");
+            return "rock"
+        case 1:
+            log("Computer choose: 🖐🏻");
+            return "paper"
+        case 2:
+            log("Computer choose: ✌🏻");
+            return "scissors"
     }
+}
 
-    function isRock(string) {
-        return (string.toLowerCase() === 'rock') ? true : false;
-    }
+function getHumanChoice() {
+    let humanChoice = prompt("Please choose from Rock, Paper, Scissors", '');
 
-    function isPaper(string) {
-        return (string.toLowerCase() === 'paper') ? true : false;
+    if (isEmpty(humanChoice)) {
+        log('You Input Empty, please Input again.');
+        // return getHumanChoice();
     }
+    else if (
+        isRock(humanChoice) ||
+        isPaper(humanChoice) ||
+        isScissors(humanChoice)) {
+        log(`You Input ${humanChoice}.`);
+        return humanChoice.toLowerCase();
+    }
+    else {
+        log("Input again!");
+        return getHumanChoice();
+    }
+}
 
-    function isScissors(string) {
-        return (string.toLowerCase() === 'scissors') ? true : false;
-    }
+function isEmpty(string) {
+    return !string
+}
+
+function isRock(string) {
+    return (string.toLowerCase() === 'rock') ? true : false;
+}
+
+function isPaper(string) {
+    return (string.toLowerCase() === 'paper') ? true : false;
+}
+
+function isScissors(string) {
+    return (string.toLowerCase() === 'scissors') ? true : false;
 }
